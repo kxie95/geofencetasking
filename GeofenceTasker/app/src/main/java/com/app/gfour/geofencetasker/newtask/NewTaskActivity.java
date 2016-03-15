6f9088fff7a3bf6defb4eac2b362ea32dc461fc8
Karen <kxie094@aucklanduni.ac.nz>
2016-03-15 17:43:42 +1300

    Added classes for handling task data storage.

---
+++ GeofenceTasker/app/src/main/java/com/app/gfour/geofencetasker/newtask/NewTaskActivity.java
@@ -0,0 +1,9 @@
+package com.app.gfour.geofencetasker.newtask;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Shows the UI for creating a new task.
+ */
+public class NewTaskActivity extends AppCompatActivity {
+}
