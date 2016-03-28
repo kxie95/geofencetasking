b6c1a0cbec438081990b509b61254afee6ae2066
Karen <kxie094@aucklanduni.ac.nz>
2016-03-29 12:28:38 +1300

    Rearranged code in SQLDB classes. Added places auto complete.

--- GeofenceTasker/app/src/main/java/com/app/gfour/geofencetasker/data/TaskHelper.java
+++ GeofenceTasker/app/src/main/java/com/app/gfour/geofencetasker/data/TaskHelper.java
@@ -3,7 +3,8 @@ package com.app.gfour.geofencetasker.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
+
+import static com.app.gfour.geofencetasker.data.TaskContract.*;
 
 /**
  * Helper for storing task data.
@@ -11,23 +12,40 @@ import android.util.Log;
 
 public class TaskHelper extends SQLiteOpenHelper {
 
+	// If you change the database schema, you must increment the database version.
+	public static final int DATABASE_VERSION = 1;
+	public static final String DATABASE_NAME = "Tasks.db";
+
+	private static final String TEXT_TYPE = " TEXT";
+	private static final String REAL_TYPE = " REAL";
+	private static final String COMMA_SEP = ",";
+
+    /**
+     * Commands to create and delete entries.
+     */
+	private static final String SQL_CREATE_ENTRIES =
+			"CREATE TABLE " + TaskEntry.TABLE_NAME + " (" +
+					TaskEntry.COLUMN_NAME_ID + " INTEGER PRIMARY KEY," +
+					TaskEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+					TaskEntry.COLUMN_NAME_LAT + REAL_TYPE + COMMA_SEP +
+					TaskEntry.COLUMN_NAME_LONG + REAL_TYPE +
+			" )";
+	private static final String SQL_DELETE_ENTRIES =
+			"DROP TABLE IF EXISTS " + TaskEntry.TABLE_NAME;
+
 	public TaskHelper(Context context) {
-		super(context, TaskContract.DB_NAME, null, TaskContract.DB_VERSION);
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
-
-	@Override
-	public void onCreate(SQLiteDatabase sqlDB) {
-		String sqlQuery = String.format("CREATE TABLE %s (" +
-						"_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-						"%s TEXT)", TaskContract.TABLE, TaskContract.Columns.TASK);
-
-		Log.d("TaskHelper","Query to form table: " + sqlQuery);
-		sqlDB.execSQL(sqlQuery);
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(SQL_CREATE_ENTRIES);
 	}
-
-	@Override
-	public void onUpgrade(SQLiteDatabase sqlDB, int i, int i2) {
-		sqlDB.execSQL("DROP TABLE IF EXISTS " + TaskContract.TABLE);
-		onCreate(sqlDB);
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// This database is only a cache for online data, so its upgrade policy is
+		// to simply to discard the data and start over
+		db.execSQL(SQL_DELETE_ENTRIES);
+		onCreate(db);
+	}
+	public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		onUpgrade(db, oldVersion, newVersion);
 	}
 }
--- GeofenceTasker/app/src/main/java/com/app/gfour/geofencetasker/newtask/NewTaskActivity.java
+++ GeofenceTasker/app/src/main/java/com/app/gfour/geofencetasker/newtask/NewTaskActivity.java
@@ -2,11 +2,18 @@ package com.app.gfour.geofencetasker.newtask;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.app.gfour.geofencetasker.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 /**
@@ -24,8 +31,9 @@ public class NewTaskActivity extends AppCompatActivity {
     /**
      * Store location here after the user selects it from gmap.
      */
-    private LatLng selectedLocation;
-    
+    private LatLng mSelectedLocation;
+    private String TAG = "NewTaskActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,20 +41,42 @@ public class NewTaskActivity extends AppCompatActivity {
 
         mTitle = (EditText) findViewById(R.id.new_task_title);
         mDescription = (EditText) findViewById(R.id.new_task_description);
-        mLocation = (EditText) findViewById(R.id.new_task_location);
+        //mLocation = (EditText) findViewById(R.id.new_task_location);
         mDoneButton = (Button) findViewById(R.id.new_tast_done_button);
 
         mDoneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO: Save title, description and location (latlong probs) in storage.
+                if (mSelectedLocation == null) {
+                    Toast.makeText(getBaseContext(), "Please select a location.", Toast.LENGTH_LONG).show();
+                } else if (mTitle.getText() == null || mTitle.getText().equals("")) {
+                    Toast.makeText(getBaseContext(), "Please give your task a title.", Toast.LENGTH_LONG).show();
+                } else {
+                    // TODO: Save details in SQLiteDB. LatLng to be stored as (double lat, double lon).
+                }
+
             }
         });
 
-        mLocation.setOnClickListener(new View.OnClickListener() {
+        // Place picker fragment
+        final SupportPlaceAutocompleteFragment autocompleteFragment = (SupportPlaceAutocompleteFragment)
+                getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
-            public void onClick(View v) {
-                // TODO: Open up google maps, get latlong and set address on textfield.
+            public void onPlaceSelected(Place place) {
+                Log.i(TAG, "Place: " + place.getName());
+                mSelectedLocation = place.getLatLng();
+                autocompleteFragment.setText(place.getAddress());
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.e(TAG, "onError: Status = " + status.toString());
+
+                Toast.makeText(getBaseContext(), status.getStatusMessage(), Toast.LENGTH_LONG)
+                        .show();
             }
         });
     }
