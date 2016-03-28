4b7417eebe7deb579a3cde059b8468edea08ac4b
Karen <kxie094@aucklanduni.ac.nz>
2016-03-29 10:32:16 +1300

    Added extra comments and added google maps dependency.

--- GeofenceTasker/app/src/main/java/com/app/gfour/geofencetasker/newtask/NewTaskActivity.java
+++ GeofenceTasker/app/src/main/java/com/app/gfour/geofencetasker/newtask/NewTaskActivity.java
@@ -2,10 +2,12 @@ package com.app.gfour.geofencetasker.newtask;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.app.gfour.geofencetasker.R;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Shows the UI for creating a new task.
@@ -14,11 +16,16 @@ import com.app.gfour.geofencetasker.R;
  */
 public class NewTaskActivity extends AppCompatActivity {
 
-    EditText mTitle;
-    EditText mDescription;
-    EditText mLocation;
-    Button mDoneButton;
+    private EditText mTitle;
+    private EditText mDescription;
+    private EditText mLocation;
+    private Button mDoneButton;
 
+    /**
+     * Store location here after the user selects it from gmap.
+     */
+    private LatLng selectedLocation;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +35,19 @@ public class NewTaskActivity extends AppCompatActivity {
         mDescription = (EditText) findViewById(R.id.new_task_description);
         mLocation = (EditText) findViewById(R.id.new_task_location);
         mDoneButton = (Button) findViewById(R.id.new_tast_done_button);
+
+        mDoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Save title, description and location (latlong probs) in storage.
+            }
+        });
+
+        mLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Open up google maps, get latlong and set address on textfield.
+            }
+        });
     }
 }
