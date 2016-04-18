a570560050c7862c56eff1364d5d8238c165852c
IcyCaster <Chester.booker.nz@gmail.com>
2016-04-18 19:36:21 +1200

    Continent + 3inARow Achievements added, no notifications

--- GeofenceTasker/app/src/main/java/com/app/gfour/geofencetasker/data/GeofenceIntentService.java
+++ GeofenceTasker/app/src/main/java/com/app/gfour/geofencetasker/data/GeofenceIntentService.java
@@ -74,14 +74,14 @@ public class GeofenceIntentService extends IntentService {
                     triggeredGeofences);
             
             // Send notification and log the transition details.
-            sendNotification(transitionDetails);
+            sendNotification(getTransitionString(geofenceTransition), transitionDetails);
         } else {
             Log.e(TAG, "GeofenceTransition" + geofenceTransition);
         }
 
     }
 
-    private void sendNotification(String transitionDetails) {
+    private void sendNotification(String transition, String transitionDetails) {
         // Create an explicit content Intent that starts the main Activity.
         Intent notificationIntent = new Intent(getApplicationContext(), TasksActivity.class);
 
@@ -103,8 +103,8 @@ public class GeofenceIntentService extends IntentService {
 
         // Define the notification settings.
         builder.setSmallIcon(R.drawable.ic_media_play)
-                .setContentTitle(transitionDetails)
-                //.setContentText(getString(R.string.geofence_transition_notification_text))
+                .setContentTitle(transition)
+                .setContentText(transitionDetails)
                 .setContentIntent(notificationPendingIntent);
 
         // Dismiss notification once the user touches it.
