5fa7f985908c6cc7f6c1b8c0a9760933bd2395bc
Karen <kxie094@aucklanduni.ac.nz>
2016-04-21 00:04:07 +1200

    Class renamer now finds which files are Android components.

--- StringEncryptor/src/classPackageObfuscate/ClassRenamer.java
+++ StringEncryptor/src/classPackageObfuscate/ClassRenamer.java
@@ -1,11 +1,40 @@
 package classPackageObfuscate;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.io.FileUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+class Start {
+	public static void main(String[] args) {
+		ClassRenamer cr = new ClassRenamer();
+		cr.renameClasses("C:\\Users\\karen\\Desktop\\Android  Studio Projects\\groupfour\\TaskerProguardTest");
+	}
+}
 /**
  * Class which renames classes which extend of Android components.
  * These include Activity, Service, IntentService, BroadcastReceiver
@@ -14,49 +43,154 @@ import java.util.Set;
 public class ClassRenamer {
 	
 	// Set with list of possible Android components
-	private static Set<String> androidComponents = new HashSet<String>(); 
-	
+	private Set<String> androidComponents = new HashSet<String>(); 
 	// Used to generate a random string.
-	private static SecureRandom random = new SecureRandom();
-
+	private SecureRandom random = new SecureRandom();
+	// Relative path to AndroidManifest file.
+	private static final String PATH_TO_MANIFEST = "\\app\\src\\main\\AndroidManifest.xml";
+	// Relative path to java src code.
+	private static final String PATH_TO_SRC = "\\app\\src\\main\\java";
+	// Name of the top level package.
+	String packageName = "";
+	Map<String, String> componentNames;
 	
-	public static void renameClasses(String copiedFileLocation) {
+	/**
+	 * Renames Android component classes and their references to a random String.
+	 * @param rootDir Path of the root directory of the project.
+	 */
+	public void renameClasses(String rootDir) {
+		// Add possible Android components to a hashset to be used to find elements in the XML.
 		addComponents();
 		
-		File rootDirectory = new File(copiedFileLocation);
-		
-		// Recursively look through files
-		File file = new File(""); // TODO: Get each file.
-		
-		// If the java file is an Android component
-		if (isAndroidComponent(file)) {
-			// Get the name of the class.
-			String name = file.getName().replace(".java", "");
+		File file = new File(rootDir);
+		try {
+			// Parse AndroidManifest.xml
+			Document xmlDoc = parseXMLfile(file.getAbsolutePath() + PATH_TO_MANIFEST);
 			
-			// Generate a random obfuscated name.
-			String randomName = generateRandomName();
+			// Get the top level package name.
+			packageName = getPackageName(xmlDoc);
 			
-			// Replace references of the old name with the new one.
-			renameClassAndReferences(name, randomName);
+			// Get the Android components to be renamed.
+			componentNames = getComponentNames(xmlDoc);
+			for (String s : componentNames.keySet()) {
+				System.out.println(s + " " + componentNames.get(s));
+			}
+			
+			renameFilesAndRefs(rootDir + PATH_TO_SRC);
+			
+			
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} catch (SAXException saxe) {
+			saxe.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
 		}
+		
 	}
 	
-	private static void renameClassAndReferences(String name, String randomName) {
-		// TODO Auto-generated method stub
+	private void renameFilesAndRefs(String srcPath) throws FileNotFoundException {
+		File dir = new File(srcPath);
+		File [] files = dir.listFiles();
+		for (File f : files) {
+			// Keep looking if is directory.
+			if (f.isDirectory()) {
+				renameFilesAndRefs(f.getAbsolutePath());
+			} else { // Else, it's a java file.
+				System.out.println(f.getName());
+				replaceReferences(f);
+			}
+		}
 		
 	}
 
-	private static String generateRandomName() {
-		String randomString = new BigInteger(130, random).toString(32);
+	private void replaceReferences(File file) throws FileNotFoundException {
+	    Scanner scanner = new Scanner(file);
+	    boolean isComponent = false;
+	    while (scanner.hasNextLine()) {
+	    	String line = scanner.nextLine();
+	    	if (line.startsWith("package")) {
+	    		line = line.replace("package ", "");
+	    		line.replace(";", "");
+	    		if (componentNames.containsKey(line)) {
+	    			isComponent = true;
+	    		}
+	    	}
+	    	
+	    	
+	    }
+		// Check if it's a component file
+		//if (componentNames.containsKey(file.getAbsolutePath()))
+		
+		// Else it's not a compoenent, but check if it has references.
+		
+	}
+
+	private Map<String, String> getComponentNames(Document xmlDoc) {
+		Map<String, String> componentNames = new HashMap<String, String>();
+		
+		// Get list of Android components
+		for (String componentName : androidComponents) {
+			// Get the component. Continue if it doesn't exist.
+			NodeList nl = xmlDoc.getElementsByTagName(componentName);
+			if (nl == null) {
+				continue;
+			}
+			
+			// Get the declared name of the component.
+			for (int i = 0; i < nl.getLength(); i++) {
+				NamedNodeMap attributes = nl.item(i).getAttributes();
+				String nameValue = attributes.getNamedItem("android:name").getNodeValue();
+				if (nameValue.startsWith(".")) {
+					componentNames.put(packageName + nameValue, generateRandomName());
+				} else {
+					componentNames.put(nameValue, generateRandomName());
+				}
+			}
+		}
+		
+		return componentNames;
+	}
+	
+	/**
+	 * Gets the package name declared in the Android Manifest xml.
+	 * @param xmlDoc The parsed XML file.
+	 * @return the package name
+	 */
+	private String getPackageName(Document xmlDoc) {
+		return xmlDoc.getDocumentElement().getAttribute("package");
+	}
+	
+	/**
+	 * Parses an XML file given a file path.
+	 * @param filePath Absolute path of the file.
+	 * @return Document An XML representation of the document.
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
+	private Document parseXMLfile(String filePath) throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(filePath);
+		doc.getDocumentElement().normalize();
+		return doc;
+	}
+
+	private String generateRandomName() {
+		String randomString = new BigInteger(32, random).toString(32);
 		// To ensure it begins with a letter
-		return "Z" + randomString;
+		Random r = new Random();
+		char randomChar = (char) (r.nextInt(26) + 'a');
+
+		return randomChar + randomString;
 	}
 
 	/**
 	 * Method which checks a java file to see if it is an Android component.
 	 * @return true if the file is an Android component.
 	 */
-	private static boolean isAndroidComponent(File file) {
+	private boolean isAndroidComponent(File file) {
 		// Use regex to find extending class as a string.
 		String extendingClass = "";
 		if (androidComponents.contains(extendingClass)) {
@@ -68,11 +202,10 @@ public class ClassRenamer {
 	/**
 	 * Adds Android components to a hash set as Strings.
 	 */
-	private static void addComponents() {
-		androidComponents.add("Activity");
-		androidComponents.add("Service");
-		androidComponents.add("IntentService");
-		androidComponents.add("ContentProvider");
-		androidComponents.add("BroadcastReceiver");
+	private void addComponents() {
+		androidComponents.add("activity");
+		androidComponents.add("service");
+		androidComponents.add("provider");
+		androidComponents.add("receiver");
 	}
 }
